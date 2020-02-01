f5a5688a365c8058dbe4c8968516fc15c675c67a
Szanyi Krisztián <szkript@gmail.com>
2020-02-01 23:09:46 +0100

    Calculate Side positions by coordinate(y) and add to dictionary according to side

--- GameObjects/Brick.cs
+++ GameObjects/Brick.cs
@@ -1,5 +1,8 @@
 ﻿namespace Pong
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Numerics;
     using System.Windows.Media;
     using System.Windows.Shapes;
@@ -10,6 +13,7 @@
         public double Height { get; set; } = 20;
         public Rectangle brick { get; set; }
         public Vector2 Position;
+        public Dictionary<Side, List<int>> sides = new Dictionary<Side, List<int>>();
 
         public Brick(Vector2 position)
         {
@@ -21,10 +25,20 @@
             rect.Height = Height;
             brick = rect;
             Position = position;
+            CalculateSides();
         }
-        public void GetSideCoordinates()
+
+        private void CalculateSides()
         {
-            
+            List<int> topSide = Enumerable.Range((int)Position.Y, (int)Width).ToList();
+            List<int> rightSide = Enumerable.Range((int)Position.Y + (int)Width, (int)Height).ToList();
+            List<int> bottomSide = Enumerable.Range((int)Position.Y + (int)Height, (int)Width).ToList();
+            List<int> leftSide = Enumerable.Range((int)Position.Y, (int)Height).ToList();
+
+            sides.Add(Side.Top, topSide);
+            sides.Add(Side.Right, rightSide);
+            sides.Add(Side.Bottom, bottomSide);
+            sides.Add(Side.Left, leftSide);
         }
     }
     public enum Side
