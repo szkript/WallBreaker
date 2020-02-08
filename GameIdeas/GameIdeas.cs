ebbc5050789015443d3eab6a2335eddcb4dd7eb0
Szanyi Krisztián <szkript@gmail.com>
2020-02-08 16:15:41 +0100

    GameIdeas for filling with random ideas as TODO

---
+++ GameIdeas/GameIdeas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallBreaker.GameIdeas
+{
+    class GameIdeas
+    {
+        //TODO: down arrow slowing down the ball for max 1 sec
+    }
+}
